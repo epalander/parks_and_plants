5d8ed33c95964a98322ee04e5671d7e4057de0a8
Alexandre Bouvier <alexandre.b2506@gmail.com>
2020-04-16 17:52:11 +0200

    As a user I can add a plant in a garden

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 - [x] As a user I can see one garden
 - [x] As a user I can see one garden's plants
-- [ ] As a user I can add a plant in a garden
+- [x] As a user I can add a plant in a garden
 - [ ] As a user I can delete a plant
 
 ### Steps
---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,23 @@
+class PlantsController < ApplicationController
+  def new
+    @garden = Garden.find(params[:garden_id])
+    @plant  = Plant.new
+  end
+
+  def create
+    @plant  = Plant.new(plant_params)
+    @garden = Garden.find(params[:garden_id])
+    @plant.garden = @garden
+    if @plant.save
+      redirect_to garden_path(@garden)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:species, :picture)
+  end
+end
