073146193dfb0c44558b28a26a4f7f05931a92fc
Alexandre Bouvier <alexandre.b2506@gmail.com>
2020-04-17 16:38:48 +0200

    As a user I can see index garden

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,4 +1,8 @@
 class GardensController < ApplicationController
+  def index
+    @gardens = Garden.all
+  end
+
   def show
     @garden = Garden.find(params[:id])
     @plants = @garden.plants
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'plants/new'
-  get 'plants/create'
-  get 'gardens/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :gardens, only: [:show] do
+  root to: "gardens#index"
+  resources :gardens, only: [:index, :show] do
     resources :plants, only: [:new, :create]
   end
   resources :plants, only: [:destroy]
